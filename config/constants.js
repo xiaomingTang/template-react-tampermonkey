2cee419f76dfa1a58ebe25684c887186dd0b5f7b
xiaomingTang <1038761793@qq.com>
2020-12-27 20:44:18 +0800

    添加更详细的注释

--- config/constants.js
+++ config/constants.js
@@ -2,16 +2,21 @@ const { Json } = require("tang-base-node-utils")
 
 const packageJson = new Json(__dirname, "../package.json").readSync()
 
+// -----------------------------------
+// 以下为自定义区, 如需修改请注意注释项
+// -----------------------------------
+
 /**
- * src 中用到的 tampermonkey 内置函数, 均需要在 prefix 中声明 @grant
- * 
- * externals 中用到的包, 应当
- * 1. 在 prefix 中声明 @require
- * 2. 执行 yarn add *** --dev 将之作为 devDependencies, 以便在 development 环境下运行
+ * 一. src 中用到的 tampermonkey 内置函数, 均需要在 prefix 中声明 @grant
+ *
+ * 二. externals 中用到的包, 应当
+ *   1. 在 prefix 中声明 @require
+ *   2. 下面的 const externals 中声明
+ *   3. 执行 yarn add -D *** 将之作为 devDependencies, 以便在 development 环境下运行
  */
 const prefix = `
 // ==UserScript==
-// @timestamp    ${Date.now()/* 这个参数用来确定你确实成功更新了脚本 */}
+// @timestamp    ${Date.now()/* 这个参数类似于版本号, 你可以注意该参数, 以证明你确实更新了该脚本 */}
 // @name         ${packageJson.name || "tamperscript name"}
 // @author       ${packageJson.author || "tamperscript author"}
 // @version      ${packageJson.version || "1.0.0"}
@@ -27,6 +32,9 @@ const prefix = `
 // ==/UserScript==
 `
 
+/**
+ * externals 中用到的包, 应当在此处声明
+ */
 const externals = {
   "react" : "React",
   "react-dom" : "ReactDOM",
